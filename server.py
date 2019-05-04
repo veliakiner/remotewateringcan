8e8e6e2eb27c28512a1a6443ee9233751ec0392d
Veli Akiner <veli@kopernio.com>
2019-05-04 15:10:28 +0100

    initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from subprocess import check_call
+import time
+
+BASE_RELAY_CMD = "sudo usbrelay V5ZEA_2={}"
+app = Flask(__name__)
+
+@app.route('/water')
+def hello_world():
+    try:
+        check_call(BASE_RELAY_CMD.format(1).split(" "))
+        time.sleep(5)
+    finally:
+        check_call(BASE_RELAY_CMD.format(0).split(" "))
