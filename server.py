0c0cb869495e39f18d14bf466b21fcd5e557cbcf
Veli Akiner <veli@kopernio.com>
2019-05-19 14:14:11 +0100

    .

--- server.py
+++ server.py
@@ -1,15 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 from subprocess import check_call
 import time
-
+import os
 BASE_RELAY_CMD = "sudo usbrelay V5ZEA_2={}"
 app = Flask(__name__)
+
+
+def start_watering():
+    if os.environ.get("MOCK_WATER"):
+        print("No hardware interface")
+        return
+    return check_call(BASE_RELAY_CMD.format(1).split(" "))
+
+
+def stop_watering():
+    if os.environ.get("MOCK_WATER"):
+        print("No hardware interface")
+        return
+    return check_call(BASE_RELAY_CMD.format(0).split(" "))
+
 
 @app.route('/water')
 def hello_world():
     try:
-        check_call(BASE_RELAY_CMD.format(1).split(" "))
+        start_watering()
         time.sleep(5)
     finally:
-        check_call(BASE_RELAY_CMD.format(0).split(" "))
+        stop_watering()
     return "Success"
+
+
+@app.route("/")
+def main():
+    return render_template("home.html")
