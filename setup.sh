e0a963f5a02e2a984ab2e429fd62066a7454396a
Veli Akiner <veli@kopernio.com>
2019-05-19 23:17:20 +0100

    download correct ngrok

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 
 sudo apt-get install -y usbrelay
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ng.zip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -O ng.zip
 unzip ng.zip -d ./
 ./ngrok authtoken 5aoC3eKttoW1LXLDv4YbT_2qbyphConKdYSv4HckbCD
